0eeae1f11e03e0d7dd2c70947c4812e3582af3a4
1234 <a1234@1234s-MacBook-Pro.local>
2026-01-20 01:43:57 +0500

    leap year

--- calendar_generator.py
+++ calendar_generator.py
@@ -3,8 +3,11 @@ class CalendarGenerator:
         pass
 
 
-    def is_leap_year(year):
-        pass
+    def is_leap_year(self,year):
+        if (year%4==0 and year%100!=0) or (year%400==0):
+            return True
+        else:
+            return False
 
     def get_days_in_month(month,year):
         pass
@@ -17,6 +20,9 @@ class CalendarGenerator:
     def build_grid_string(start_day_index,total_days):
         pass
 
-    
+
     def generate_calendar(month,year):
         pass
+if __name__ == "__main__":
+    Cg = CalendarGenerator()
+    print(Cg.is_leap_year(2023))
