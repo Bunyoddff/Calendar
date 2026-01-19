0660c4536c8c2a07efdb71d5baabf0cef8ff74c8
1234 <a1234@1234s-MacBook-Pro.local>
2026-01-20 01:09:28 +0500

    first commit for function

---
+++ calendar_generator.py
@@ -0,0 +1,22 @@
+class CalendarGenerator:
+    def __init__(self):
+        pass
+
+
+    def is_leap_year(year):
+        pass
+
+    def get_days_in_month(month,year):
+        pass
+
+
+    def get_start_day_of_month(month,year):
+        pass
+
+
+    def build_grid_string(start_day_index,total_days):
+        pass
+
+    
+    def generate_calendar(month,year):
+        pass
