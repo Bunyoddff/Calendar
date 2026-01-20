7206b21c9a9c369dd54eba2fb282c9996658b5b4
1234 <a1234@1234s-MacBook-Pro.local>
2026-01-20 07:34:21 +0500

    get days in month function

--- calendar_generator.py
+++ calendar_generator.py
@@ -4,13 +4,19 @@ class CalendarGenerator:
 
 
     def is_leap_year(self,year):
-        if (year%4==0 and year%100!=0) or (year%400==0):
-            return True
-        else:
-            return False
+        return (year%4==0 and year%100!=0) or (year%400==0)
 
-    def get_days_in_month(month,year):
-        pass
+    def get_days_in_month(self,month,year):
+        mon31=[1,3,5,7,8,10,12]
+        mon30=[4,6,9,11]
+        if month in mon31:
+            return 31
+        elif month in mon30:
+            return 30
+        elif self.is_leap_year(year) and month==2:
+            return 28
+        else:
+            return 29
 
 
     def get_start_day_of_month(month,year):
@@ -25,4 +31,5 @@ class CalendarGenerator:
         pass
 if __name__ == "__main__":
     Cg = CalendarGenerator()
-    print(Cg.is_leap_year(2023))
+    print(Cg.is_leap_year(2024))
+    print(Cg.get_days_in_month(2,2024))
